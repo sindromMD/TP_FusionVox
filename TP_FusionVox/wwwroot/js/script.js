5d8cc0014fe33d0cbf1db30ee09633a3eb279faa
Culaxiz Alexei <6217367@cegepmontpetit.ca>
2023-08-04 03:28:30 -0400

    REFACTOR Placement des boutons dans les vuesPartielles
    - Création de viewPartielle pour les boutons de la vue :
        * Artiste (Delete/Detail/Upsert),
        * Home (Delete / Upsert)
    - Ajout du bouton éditer aux cartes d'artistes
    - Ajout de fonctions de modification des règles css ( JS)
    - Modification des règles CSS, et _Layout

---
+++ TP_FusionVox/wwwroot/js/script.js
@@ -0,0 +1,28 @@
+
+$(document).ready(function(){
+    //formatage de l'adresse en petits ou grands caractères pour éviter les erreurs
+    var pathToLower = window.location.pathname ? window.location.pathname.toLowerCase() : "";
+    
+    // vérification si l'adresse contient :create/edit et genremusical
+    //changer en<i> à l'intérieur du <button> de type submit, class "fa-user-.."
+    // l'icône dans la dropdown(header) restera intacte.
+
+    if(pathToLower.includes("genremusical")){
+        if(pathToLower.includes("create"))
+            $("button[type='submit'] i.fa-user-plus").removeClass("fa-user-plus").addClass("fa-plus");
+        else
+            $("button[type='submit'] i.fa-user-pen").removeClass("fa-user-pen").addClass("fa-pen-to-square"); 
+    }
+
+    // réutilisation de viewPartial[_FormAjoutAuFavorisButtonPartielle] pour 
+    //l'ajouter aux favoris (modifier la taille et la position du bouton))
+    if(pathToLower.includes("detail"))
+    {
+        $("div.btn-ajouter-coeur-card-position").css({
+            "top": "15px",
+            "right": "25px"
+        });
+        $("button.fa-heart-circle-plus").removeClass("fa-xl").addClass("fa-2xl");
+    }
+        
+})
